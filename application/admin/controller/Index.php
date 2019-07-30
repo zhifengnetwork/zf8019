d50432bb75abe502452e6c6bcdb4b1c74ff59a7e
sonelyx <snowxtlyx@163.com>
2019-07-30 16:22:54 +0800

    gengxin

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\admin\controller;
 
 
@@ -16,91 +17,95 @@ class Index extends Common
     public function index()
     {
         $orderNo = 121321;
-        $Sales = new Sales(27871,132123,0);
-        $rest = $Sales->reward_fifty(27871,$orderNo,$orderNo,30);
-        die;
+        $Sales = new Sales(27871, 132123, 0);
+        $rest = $Sales->reward_fifty(27871, $orderNo, $orderNo, 30);
+        // die;
         $this->assign('meta_title', '首页');
         return $this->fetch();
     }
 
 
-    public function page_edit(){
-        $id  = request()->param('id',0,'intval');
-        $this->assign('id',$id);
+    public function page_edit()
+    {
+        $id  = request()->param('id', 0, 'intval');
+        $this->assign('id', $id);
         $this->assign('meta_title', '页面编辑');
         return $this->fetch();
     }
 
 
-    public function page_add(){
+    public function page_add()
+    {
         $this->assign('meta_title', '页面新增');
         return $this->fetch();
     }
 
-    public function status(){
-        
-    }
+    public function status()
+    { }
 
-    public function page_enable () {
-        $id = request()->param('id',0,'intval');
-        $status = request()->param('status',0,'intval');
-        if (!empty($id)){
-            $getPage = model('DiyEweiShop')->where(['id'=>$id])->find();
-            if (!empty($getPage)){
-                if ($getPage['status'] == $status){
-                    if ($status == 0){
-                        return json(['code'=>0, 'msg'=>'该页面已经是禁用了','data'=>[]]);
-                    }else{
-                        return json(['code'=>0, 'msg'=>'该页面已经是启用了','data'=>[]]);
+    public function page_enable()
+    {
+        $id = request()->param('id', 0, 'intval');
+        $status = request()->param('status', 0, 'intval');
+        if (!empty($id)) {
+            $getPage = model('DiyEweiShop')->where(['id' => $id])->find();
+            if (!empty($getPage)) {
+                if ($getPage['status'] == $status) {
+                    if ($status == 0) {
+                        return json(['code' => 0, 'msg' => '该页面已经是禁用了', 'data' => []]);
+                    } else {
+                        return json(['code' => 0, 'msg' => '该页面已经是启用了', 'data' => []]);
                     }
-                }else{
-                    if ($status == 1){
-                        $getThisEnablePage = model('DiyEweiShop')->where(['status'=>1])->find();
-                        if (!empty($getThisEnablePage)){
-                            model('DiyEweiShop')->where(['id'=>$getThisEnablePage['id']])->update(['status'=>0]);
+                } else {
+                    if ($status == 1) {
+                        $getThisEnablePage = model('DiyEweiShop')->where(['status' => 1])->find();
+                        if (!empty($getThisEnablePage)) {
+                            model('DiyEweiShop')->where(['id' => $getThisEnablePage['id']])->update(['status' => 0]);
                         }
                     }
-                    $updateThisPage = model('DiyEweiShop')->where(['id'=>$id])->update(['status'=>$status]);
-                    if ($updateThisPage){
-                        return json(['code'=>1, 'msg'=>'操作成功','data'=>[]]);
-                    }else{
-                        return json(['code'=>0, 'msg'=>'操作失败','data'=>[]]);
+                    $updateThisPage = model('DiyEweiShop')->where(['id' => $id])->update(['status' => $status]);
+                    if ($updateThisPage) {
+                        return json(['code' => 1, 'msg' => '操作成功', 'data' => []]);
+                    } else {
+                        return json(['code' => 0, 'msg' => '操作失败', 'data' => []]);
                     }
                 }
-            }else{
-                return json(['code'=>0, 'msg'=>'页面不存在！','data'=>[]]);
+            } else {
+                return json(['code' => 0, 'msg' => '页面不存在！', 'data' => []]);
             }
-        }else{
-            return json(['code'=>0, 'msg'=>'id不存在','data'=>[]]);
+        } else {
+            return json(['code' => 0, 'msg' => 'id不存在', 'data' => []]);
         }
     }
 
-    public function page_delete () {
-        $id = request()->param('id',0,'intval');
+    public function page_delete()
+    {
+        $id = request()->param('id', 0, 'intval');
         if (!empty($id)) {
             $getPage = model('DiyEweiShop')->where(['id' => $id])->find();
-            if (!empty($getPage)){
-                $delete = model('DiyEweiShop')->where(['id'=>$id])->update(['status'=>-1]);
-                if ($delete){
-                    return json(['code'=>1, 'msg'=>'操作成功','data'=>[]]);
-                }else{
-                    return json(['code'=>0, 'msg'=>'操作失败','data'=>[]]);
+            if (!empty($getPage)) {
+                $delete = model('DiyEweiShop')->where(['id' => $id])->update(['status' => -1]);
+                if ($delete) {
+                    return json(['code' => 1, 'msg' => '操作成功', 'data' => []]);
+                } else {
+                    return json(['code' => 0, 'msg' => '操作失败', 'data' => []]);
                 }
-            }else{
-                return json(['code'=>0, 'msg'=>'页面不存在！','data'=>[]]);
+            } else {
+                return json(['code' => 0, 'msg' => '页面不存在！', 'data' => []]);
             }
-        }else{
-            return json(['code'=>0, 'msg'=>'id不存在','data'=>[]]);
+        } else {
+            return json(['code' => 0, 'msg' => 'id不存在', 'data' => []]);
         }
     }
 
     /***
      * 支付方式
      */
-    public function pay_wechat(){
+    public function pay_wechat()
+    {
         $sysset = Db::table('sysset')->find();
         $set    = unserialize($sysset['sets']);
-        if( Request::instance()->isPost() ){
+        if (Request::instance()->isPost()) {
             $data = input('post.');
             $set['pay']['weixin'] = $data['pay']['weixin'];
             $set['pay']['credit'] = $data['pay']['credit'];
@@ -113,7 +118,7 @@ class Index extends Common
             $set['wechat']['apikey']       =  $data['wechat']['apikey'];
             $update['sets']    =   serialize($set);
             $res = Db::table('sysset')->where(['id' => $sysset['id']])->update($update);
-            if($res){
+            if ($res) {
                 $this->success('编辑成功', url('index/pay_wechat'));
             }
             $this->error('编辑失败');
@@ -126,10 +131,11 @@ class Index extends Common
     /***
      * 支付宝
      */
-    public function pay_alipay(){
+    public function pay_alipay()
+    {
         $sysset = Db::table('sysset')->find();
         $set    = unserialize($sysset['sets']);
-        if(Request::instance()->isPost()){
+        if (Request::instance()->isPost()) {
             $data         = input('post.');
             $set['pay']['alipay'] = $data['pay']['alipay'];
             $set['pay']['credit'] = $data['pay']['credit'];
@@ -141,7 +147,7 @@ class Index extends Common
             $set['alipay']['notify_url']   =  $data['alipay']['notify_url'];
             $update['sets']  = serialize($set);
             $res = Db::table('sysset')->where(['id' => $sysset['id']])->update($update);
-            if($res !== false ){
+            if ($res !== false) {
                 $this->success('编辑成功', url('index/pay_alipay'));
             }
             $this->error('编辑失败');
@@ -153,44 +159,45 @@ class Index extends Common
     /**
      * 支付交易设置
      */
-    public function pay_py(){
-            $sysset     = Db::table('sysset')->field('*')->find();
-            $set        = unserialize($sysset['sets']);
-        if(Request::instance()->isPost()){
+    public function pay_py()
+    {
+        $sysset     = Db::table('sysset')->field('*')->find();
+        $set        = unserialize($sysset['sets']);
+        if (Request::instance()->isPost()) {
             $trade = input('post.');
-            
+
             $set['trade']   = $trade['trade'];
-            
+
             $sysset['sets'] = serialize($set);
-            
+
             $res = Db::table('sysset')->where(['uniacid' => 3])->update($sysset);
-            if($res !== false ){
+            if ($res !== false) {
                 $this->success('编辑成功', url('index/pay_py'));
             }
             $this->error('编辑失败');
         }
-    
+
         $this->assign('set', $set);
         $this->assign('meta_title', '支付交易设置');
         return $this->fetch();
-
     }
     /**
      * 商城提醒
      */
-    public function notice(){
+    public function notice()
+    {
         $sysset     = Db::table('sysset')->field('*')->find();
         $set        = unserialize($sysset['sets']);
-      
-        if(Request::instance()->isPost()){
+
+        if (Request::instance()->isPost()) {
             $notice          = input('post.');
 
             $set['notice']   = $notice['notice'];
-            
+
             $sysset['sets'] = serialize($set);
-            
+
             $res = Db::table('sysset')->where(['uniacid' => 3])->update($sysset);
-            if($res !== false ){
+            if ($res !== false) {
                 $this->success('编辑成功', url('index/notice'));
             }
             $this->error('编辑失败');
@@ -200,6 +207,4 @@ class Index extends Common
         $this->assign('meta_title', '支付交易设置');
         return $this->fetch();
     }
-
-
 }
