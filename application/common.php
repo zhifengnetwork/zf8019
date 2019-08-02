39a50934b1f95f258c5214caed5e4c8d0f5e9e6e
sonelyx <snowxtlyx@163.com>
2019-08-02 23:12:48 +0800

    修改等级

--- application/common.php
+++ application/common.php
@@ -198,7 +198,7 @@ function checkMobile($mobilePhone)
     }
 }
 
-<<<<<<< HEAD
+
 function send_zhangjun($mobile, $code)
 { //掌骏
 
@@ -209,17 +209,6 @@ function send_zhangjun($mobile, $code)
     $signPars = '';
     foreach ($arr as $v) {
         $signPars .= $v;
-=======
-function send_zhangjun($mobile,$code){//掌骏
-    
-    $content = "【8019】您的手机验证码为：".$code."，该短信1分钟内有效。如非本人操作，可不用理会！";
-    $time=date('ymdhis',time());
-//    $arr=array('uname'=>"hsxx40",'pwd'=>"hsxx40",'time'=>$time);
-    $arr=array('uname'=>"qx3983",'pwd'=>"20190716",'time'=>$time);
-    $signPars='';
-    foreach($arr as $v) {
-        $signPars .=$v;
->>>>>>> 55a4ff04e4079b79c0a70a990f009c3b0030768f
     }
     $sign = strtolower(md5($signPars));
     //    $arrs=array('userid'=>"9795",'timestamp'=>$time,'sign'=>$sign,'mobile'=>$mobile,'content'=>$content,'action'=>'send');
@@ -1009,8 +998,9 @@ function uploadTou($image)
  * @param int $tel
  * @return string
  */
-function doPhone($tel){
-     $new_tel = preg_replace('/(\d{3})\d{4}(\d{4})/', '$1****$2', $tel);  
+function doPhone($tel)
+{
+    $new_tel = preg_replace('/(\d{3})\d{4}(\d{4})/', '$1****$2', $tel);
     return $new_tel;
 }
 
@@ -1063,4 +1053,3 @@ function getArrSet($arrs, $_current_index = -1)
     }
     return $_total_arr;
 }
-
