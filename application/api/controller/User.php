a6d6dbbbf945125506d9340263b3afbc6b42ae20
chenguangjie111 <979230865@qq.com>
2019-08-09 10:51:53 +0800

    用户协议
    用户协议

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -1431,6 +1431,14 @@ class User extends ApiBase
         return $this->successResult($announce);
     }
 
+    //用户协议
+    public function consult()
+    {
+        $site=Db::name('site')->find();
+        return $this->successResult($site['consult']);
+    }
+
+
 
 
 }
